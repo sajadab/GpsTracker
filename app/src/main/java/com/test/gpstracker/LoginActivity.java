ba139b9104c9e6aa8e0edac1d85611d0f2d7eb7e
sajad <sajadalibakhshi@yahoo.com>
2017-04-06 08:08:45 +0430

    Initial commit

---
+++ app/src/main/java/com/test/gpstracker/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.test.gpstracker;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SAJJAD on 4/6/2017.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
